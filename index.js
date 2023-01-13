10d01806db558d6c81805adf03b7ec2710701f1e
Yarema Kulchytskyi <Yarema.Kulchytskyi@conplement.de>
2023-01-13 15:42:34 +0100

    integrate calinga plugin

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const i18next = require('i18next')
 const express = require('express')
 
-i18next.init(
-  {
-    lng: 'en',
+const { CalingaBackend, CalingaBackendOptions } = require('i18next-calinga-backend') ;
+
+// https://manage.calinga.io/translations/SdkSample/Default%20Team/Landings%20Page/de
+
+const backendOptions = {
+    organization: 'SdkSample',
+    team: 'Default Team',
+    project: "Landings Page",
     resources: {
-      en: {
-        translation: {
-          key: 'value of key'
+        'en': {
+            'default': 'en'
+        },
+        'de': {
+            'default': 'de'
         }
-      },
-      de: {
-        translation: {
-          key: 'Wert von Key'
-        }
-    }
-    }
-  }
-  
-)
+    },
+    devMode: false
+
+};
+
+i18next
+  .use(CalingaBackend)
+  .init({
+    backend: backendOptions,
+    
+    supportedLngs: ['en', 'de']
+  });
+
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.json( { Hello: 'World!' } ));
 
-app.get('/translation/:key', (req, res) => res.json(i18next.t(req.params.key, {lng: req.query.lang})));
+app.get('/translation/:key', (req, res) => {
+  i18next.changeLanguage(req.query.lang, (error, t) => {
+    if(error){
+      res.json('error changing language', error)
+    }
+    else{
+      res.json(t(req.params.key))
+    }
+})});
 
 app.listen(
   port, 
